6066c878bd280d986fca6228334118e5cb1da835
DanVev <vasily.danilin@yandex.ru>
2017-11-10 17:28:28 +0300

    fix SVG context

--- src/Visual/SVGContext.java
+++ src/Visual/SVGContext.java
@@ -28,26 +28,39 @@ public class SVGContext implements IGContext {
 
     @Override
     public void drawLine(ICurve c, IPoint a, IPoint b) {
-        content.add(String.format("<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" stroke=\"rgb(0,0,0)\"/>", (int) a.getX(), (int) a.getY(), (int) b.getX(), (int) b.getY()));
+        int old_x = 0;
+        int old_y = 0;
+        for (double i = 0; i <= 1; i += 0.05) {
+            if ((i + 0.05 > 1) && (i != 1))
+                i = 1 - 0.05;
+                IPoint point = c.getPoint(i);
+                int x = (int) point.getX();
+                int y = (int) point.getY();
+                if (i != 0)
+                    content.add(String.format("<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" stroke=\"rgb(0,0,0)\"/>", old_x, old_y, x, y));
+                old_x = x;
+                old_y = y;
+
+        }
     }
 
     @Override
     public void drawCircle(IPoint a, int radius) {
-        content.add(String.format(" <circle cx=\"%d\" cy=\"%d\" r=\"%d\"/>",(int)a.getX(),(int)a.getY(),radius));
+        content.add(String.format(" <circle cx=\"%d\" cy=\"%d\" r=\"%d\"/>", (int) a.getX(), (int) a.getY(), radius));
     }
 
     @Override
     public void drawStartPoint(IPoint a) {
-        drawCircle(a,2);
+        drawCircle(a, 2);
     }
 
     @Override
     public void drawEndPoint(IPoint a) {
-        drawCircle(a,2);
+        drawCircle(a, 2);
     }
-    public void createSVGContent(ICurve c, IPoint a, IPoint b)
-    {
-        drawLine(c,a,b);
+
+    public void createSVGContent(ICurve c, IPoint a, IPoint b) {
+        drawLine(c, a, b);
         drawStartPoint(a);
         drawEndPoint(b);
     }
